ebebce8e14101ed13d455a26cca6632e1816350a
Maxime PEREZ <max.perez1313@gmail.com>
2020-10-04 15:44:32 +0200

    Update conf

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,27 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WebpackCleanPlugin = require('webpack-clean-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new CopyWebpackPlugin([{
-      from: 'src/index.html', to :'./index.html'
-    }]),
-    new WebpackCleanPlugin({
-        path: './dist'
-    }),
-  ],
-  devServer: {
-    host: '0.0.0.0',
-    port: 8081,
-    disableHostCheck: true,
-  }
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimize: false,
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'src/index.html', to: './index.html'
+        }]),
+        new WebpackCleanPlugin({
+            path: './dist'
+        }),
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: 8081,
+        disableHostCheck: true,
+    }
 }
 module.rules = [
     {
